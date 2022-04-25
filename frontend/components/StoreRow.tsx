f4c283495b60c797a420f1d8810c4f620dd2e413
Enias Oliveira <eniasoliveira27@gmail.com>
2022-04-25 03:19:14 -0300

    feat: adds store and transactions basic listing

---
+++ frontend/components/StoreRow.tsx
@@ -0,0 +1,80 @@
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import { Collapse, IconButton, TableCell, TableRow } from '@mui/material';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import axios from 'axios';
+import { useState } from 'react';
+import useSWR from 'swr';
+
+const fetcher = (url: string) => axios.get(url).then(res => res.data);
+
+interface StoreRowPropTypes {
+    store: {
+        id: number
+        name: string
+        owner: string
+    }
+}
+
+const StoreRow = ({ store }: StoreRowPropTypes) => {
+    const [open, setOpen] = useState(false);
+    const { data } = useSWR(() => open ? `http://strs-transactions-manager-dev.herokuapp.com/stores/${store.id}/transactions` : null, fetcher);
+
+    const transactionsColumns: GridColDef[] = [
+        {
+            field: "id",
+            headerName: "TransactionId",
+            flex: 1,
+        },
+        {
+            field: "date",
+            flex: 1,
+        },
+        {
+            field: "value",
+            flex: 1,
+        },
+        {
+            field: "CPF",
+            flex: 1,
+        },
+        {
+            field: "card",
+            flex: 1,
+        },
+    ];
+
+    return (
+        <>
+            <TableRow sx={{ '&& > *': { borderBottom: 'unset' } }} >
+                <TableCell>
+                    <IconButton
+                        aria-label="expand row"
+                        size="small"
+                        onClick={() => setOpen(!open)}
+                    >
+                        {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+                    </IconButton>
+                </TableCell>
+                <TableCell>{store.name}</TableCell>
+                <TableCell>{store.owner}</TableCell>
+            </TableRow>
+            <TableRow>
+                <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={3}>
+                    <Collapse in={open} timeout="auto" unmountOnExit>
+                        {data &&
+                            <DataGrid
+                                columns={transactionsColumns}
+                                rows={data}
+                                autoHeight
+                                sx={{ mb: 3 }}
+                            />
+                        }
+                    </Collapse>
+                </TableCell>
+            </TableRow>
+        </>
+    );
+};
+
+export { StoreRow };
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,34 +1,49 @@
-import { Typography } from '@mui/material'
+import { Container, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material';
 import axios from 'axios';
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type { NextPage } from 'next';
+import Head from 'next/head';
 import useSWR from 'swr';
+import { StoreRow } from '../components/StoreRow';
 
 const fetcher = (url: string) => axios.get(url).then(res => res.data);
 
 const Home: NextPage = () => {
-    const { data } = useSWR('http://strs-transactions-manager-dev.herokuapp.com/transactions', fetcher);
+    const { data } = useSWR('http://strs-transactions-manager-dev.herokuapp.com/stores', fetcher);
 
     if (!data) {
         return <div>...Loading</div>
     }
 
     return (
-        <div>
+        <>
             <Head>
                 <title>Stores Transactions Manager</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
             <main >
-                <Typography variant='h1'>
-                    Stores Transactions Manager
-                </Typography>
+                <Container maxWidth="lg" >
+                    <Typography variant='h3' my={3}>
+                        Stores Transactions Manager
+                    </Typography>
 
-                <div>{data}</div>
+                    <TableContainer component={Paper}>
+                        <Table aria-label="collapsible table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell></TableCell>
+                                    <TableCell>Store</TableCell>
+                                    <TableCell>Owner</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {data.map((store: { name: string, owner: string, id: number }) => (<StoreRow store={store} />))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Container>
             </main>
-
-        </div>
+        </>
     )
 }
 
