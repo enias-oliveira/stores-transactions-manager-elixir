38c3b645be48f2daaeb61d5e2caa684e6f9cb6a4
Enias Oliveira <eniasoliveira27@gmail.com>
2022-04-24 22:47:32 -0300

    feat(wip): add data fetching

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,8 +1,18 @@
 import { Typography } from '@mui/material'
+import axios from 'axios';
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import useSWR from 'swr';
+
+const fetcher = (url: string) => axios.get(url).then(res => res.data);
 
 const Home: NextPage = () => {
+    const { data } = useSWR('http://strs-transactions-manager-dev.herokuapp.com/transactions', fetcher);
+
+    if (!data) {
+        return <div>...Loading</div>
+    }
+
     return (
         <div>
             <Head>
@@ -14,6 +24,8 @@ const Home: NextPage = () => {
                 <Typography variant='h1'>
                     Stores Transactions Manager
                 </Typography>
+
+                <div>{data}</div>
             </main>
 
         </div>
