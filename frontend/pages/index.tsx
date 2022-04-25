b68628549f28888fb6c139bb4e0f29adb039ae3e
Enias Oliveira <eniasoliveira27@gmail.com>
2022-04-24 21:22:22 -0300

    chore: adds nextjs setup

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,23 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Stores Transactions Manager</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main >
+        <h1 >
+          Stores Transactions Manager
+        </h1>
+      </main>
+
+    </div>
+  )
+}
+
+export default Home
