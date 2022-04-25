07da798ecf5459cfb4e578cf4512f28c6eb9ea34
Enias Oliveira <eniasoliveira27@gmail.com>
2022-04-25 04:29:33 -0300

    fix: moves store fetching to nextjs api

--- frontend/pages/api/stores.ts
+++ frontend/pages/api/stores.ts
@@ -1,13 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import axios from 'axios'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
   name: string
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const stores = await axios.get('http://localhost:5500/stores').then(res => res.data)
+  res.status(200).json(stores)
 }
