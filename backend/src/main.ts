7ba89851510a9ac4d86e86077d3c1fa1f39600ed
Enias Oliveira <eniasoliveira27@gmail.com>
2022-06-07 16:36:27 -0300

    chore: replaces nestjs backend with phoenix elixir

--- backend/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  console.log(process.env.BACKEND_PORT);
-  await app.listen(process.env.BACKEND_PORT || 5500);
-}
-bootstrap();
